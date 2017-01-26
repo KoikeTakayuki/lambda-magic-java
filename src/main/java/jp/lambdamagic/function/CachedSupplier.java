d04da47defc202d9e2ff6f1ce20e2dcd8e278f97
KoikeTakayuki <tekasax@gmail.com>
2017-01-27 01:08:39 +0900

    Converted to Gradle project

---
+++ src/main/java/jp/lambdamagic/function/CachedSupplier.java
@@ -0,0 +1,29 @@
+package jp.lambdamagic.function;
+
+import java.util.function.Supplier;
+
+import jp.lambdamagic.NullArgumentException;
+
+public class CachedSupplier<T> implements Supplier<T> {
+	
+	private Supplier<T> wrapped;
+	private T value;
+
+	public CachedSupplier(Supplier<T> wrapped) {
+		if (wrapped == null) {
+			throw new NullArgumentException("wrapped");
+		}
+		
+		this.wrapped = wrapped;
+	}
+
+	@Override
+	public T get() {
+		if (value == null) {
+			value = wrapped.get();
+		}
+		
+		return value;
+	}
+
+}
